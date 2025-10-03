a06c93aa2310124dae7d7cb115d8940ea12b0c7e
Debbie Labedz <90341997+dlabedz@users.noreply.github.com>
2025-10-02 22:39:26 -0500

    adding page title h1 to all pages except homepage

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -53,3 +53,39 @@ function be_entry_author() {
 	$id = (int) get_the_author_meta( 'ID' );
 	echo '<p class="entry-author"><a href="' . get_author_posts_url( $id ) . '" aria-hidden="true" tabindex="-1">' . get_avatar( $id, 40 ) . '</a><em>by</em> <a href="' . get_author_posts_url( $id ) . '">' . get_the_author() . '</a></p>';
 }
+
+/**
+ * Page Title Header
+ * Displays H1 page title on all pages except front page
+ * Appears below breadcrumbs
+ */
+function be_page_title_header() {
+	// Don't display on front page
+	if ( is_front_page() ) {
+		return;
+	}
+
+	// Don't display if content has an H1 block
+	if ( be_has_h1_block() ) {
+		return;
+	}
+
+	$title = '';
+
+	// Get appropriate title based on page type
+	if ( is_singular() ) {
+		$title = get_the_title();
+	} elseif ( is_search() ) {
+		$title = sprintf( __( 'Search Results for: %s', 'nwu-2025' ), get_search_query() );
+	} elseif ( is_404() ) {
+		$title = __( 'Page Not Found', 'nwu-2025' );
+	} elseif ( is_archive() ) {
+		$title = get_the_archive_title();
+	}
+
+	// Output title if we have one
+	if ( ! empty( $title ) ) {
+		echo '<h1 class="page-title">' . wp_kses_post( $title ) . '</h1>';
+	}
+}
+add_action( 'tha_content_top', 'be_page_title_header', 15 );
