a60fe099a417a7f9abf85485173df5511316fd72
Debbie Labedz <90341997+dlabedz@users.noreply.github.com>
2025-10-11 20:27:51 -0500

    refining styles for top story block

--- blocks/top-story/render.php
+++ blocks/top-story/render.php
@@ -33,26 +33,26 @@ $excerpt = !empty($custom_excerpt) ? $custom_excerpt : get_the_excerpt($featured
 
 // Build the block output
 ?>
-<div class="block-top-story u-width-constrained">
-	<div class="block-top-story__inner">
+<div class="block-top-story">
+	<div class="block-top-story__container">
 		<div class="block-top-story__header">
 			<div class="block-top-story__icon">
 				<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/NWU-Quote-white.png'); ?>" alt="" aria-hidden="true">
 			</div>
-			<div class="block-top-story__label">Top Story</div>
 		</div>
+		<h4 class="block-top-story__label">Top Story</h4>
 
-		<h2 class="block-top-story__title">
+		<h3 class="block-top-story__title">
 			<a href="<?php echo esc_url($post_url); ?>">
 				<?php echo esc_html($post_title); ?>
-				<?php echo be_icon(array('icon' => 'chevron-large-right', 'size' => 24)); ?>
+				<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/white-arrow.svg'); ?>" alt="" aria-hidden="true">
 			</a>
-		</h2>
+		</h3>
 
 		<?php if (!empty($excerpt)): ?>
-			<div class="block-top-story__excerpt">
+			<h5 class="block-top-story__excerpt">
 				<?php echo wp_kses_post(wpautop($excerpt)); ?>
-			</div>
+			</h5>
 		<?php endif; ?>
 
 		<?php if (!empty($attribution)): ?>
