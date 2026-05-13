631e022f1ba3f5fd9e231592888fefb4bdf425db
Debbie Labedz <90341997+dlabedz@users.noreply.github.com>
2026-05-12 21:56:20 -0500

    adds some functions to help block bot traffic

--- functions.php
+++ functions.php
@@ -1534,3 +1534,28 @@ function nwu_hide_admin_bar_for_members() {
 		show_admin_bar( false );
 	}
 }
+
+// Block bot traffic
+
+add_filter( 'xmlrpc_enabled', '__return_false' );
+
+add_action( 'template_redirect', function() {
+    if ( is_author() ) {
+        wp_redirect( home_url(), 301 );
+        exit;
+    }
+});
+
+add_filter( 'rest_authentication_errors', function( $result ) {
+    if ( ! empty( $result ) ) {
+        return $result;
+    }
+    if ( ! is_user_logged_in() ) {
+        return new WP_Error(
+            'rest_not_logged_in',
+            __( 'REST API restricted to authenticated users.', 'nwu-2025' ),
+            [ 'status' => 401 ]
+        );
+    }
+    return $result;
+});
